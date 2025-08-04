20fa20e0915d5acca7ff722015968d789a0ccb9d
tovibhas1110 <tovibhas1110@gmail.com>
2025-08-04 17:05:15 +0200

    photos file name updated 12

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const AcademicProfile: React.FC = () => {
           Using finite element methods, I analyze how these factors interact, and apply the insights to optimize both structural designs and fabrication strategies tailored for metal additive manufacturing.
           </p>
           <div className="rectangle-image">
-            <img src={`${process.env.PUBLIC_URL}/images/workflow.png`} alt="Reading a book" />
+            <img src={`${process.env.PUBLIC_URL}/images/workflow.png`} alt="Reading a book" style="width:400px; height:250px; object-fit:contain;"/>
           </div>
           
         </section>
